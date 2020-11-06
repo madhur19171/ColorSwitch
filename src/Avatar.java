e3b3f1b70a6d63420f7be51c4cea762a7f4e88eb
arjun19152 <68433714+arjun19152@users.noreply.github.com>
2020-11-06 18:30:05 +0530

    Update Avatar.java

--- src/Avatar.java
+++ src/Avatar.java
@@ -28,4 +28,8 @@ public abstract class Avatar {
     public void setColor(Color color) {
         this.color = color;
     }
+    
+    // BRO this comment is by ur bro!!!!!
+    ///never delete this comment
+    /// else our bro ship will end 
 }
