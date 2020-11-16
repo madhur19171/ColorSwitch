00b56f7874c7c21977ee4f7804119834fc2ec2e9
madhur19171 <55681891+madhur19171@users.noreply.github.com>
2020-11-16 22:38:02 +0530

    update

--- src/Star.java
+++ src/Star.java
@@ -72,10 +72,17 @@ public class Star extends Avatar {
 
     @Override
     public boolean checkVicinity(Ball ball) {
+<<<<<<< Updated upstream
         //If the bottom of the star and top of ball touch, return true.
         double bottomY = cordinate.getY() + star.getScaleY() * star.getFitHeight() / 2;
         //System.out.println(bottomY);
         return ball.getTop().getY() <= bottomY;
+=======
+        //System.out.println(ball.getBall().getBoundsInParent());
+        //System.out.println(star.getBoundsInParent());
+        return ball.getBall().getBoundsInParent().intersects(star.getBoundsInParent());
+        //Fuck You
+>>>>>>> Stashed changes
     }
 
 }
